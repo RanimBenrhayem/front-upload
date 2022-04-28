dc03075b2729e2fd29b7733883f3be3fb17cf210
RanimBenrhayem <benrhayemranim6@gmail.com>
2022-04-28 02:31:15 +0200

    messyFront

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import Test from './Test'
-import './App.css';
-import Files from './Files';
-import Console from './Console';
-import Header from './Header'
-import SideBar  from './SideBar';
-import FileUploader from './Tests';
-import { UserFiles } from './UserFiles';
+import Test from "./Test";
+import "./App.css";
+import Files from "./Files";
+import Console from "./Console";
+import Header from "./Header";
+import SideBar from "./SideBar";
+import FileUploader from "./fileUpload/FileUpload";
+import { UserFiles } from "./UserFiles";
+import Print from "./Print";
+import { useReactToPrint } from "react-to-print";
+import { useRef } from "react";
+import { TestDownload } from "./TestDownload";
+import Join from "./Join";
+import Table from "./Table"
+
+
 function App() {
   return (
-    <div >
-     {/* <Test/> */}
-    {/* <Files/>  */}
-   {/* <Console/> 
+    <div>
+      
+      {/* <Test/> */}
+      {/* <Files/>  */}
+      {/* <Console/> 
     <Header/>
     <SideBar/> */}
-   {/* <FileUploader/>  */}
-   <UserFiles/>
+      {/* <FileUploader/> */}
+      <UserFiles />
+      {/* <Print/> */}
+      {/* <TestDownload/> */}
+      
+      {/* <Table/> */}
+      {/* <Join/> */}
     </div>
   );
 }
--- src/SingleFilePreview.js
+++ src/SingleFilePreview.js
@@ -1,29 +1,106 @@
+//importations
 import React from 'react'
-import { AiFillDelete,AiFillEye,AiFillEyeInvisible} from "react-icons/ai";
-import axios from "axios"
+import {AiFillEye,AiFillEyeInvisible,AiFillCloseCircle,AiOutlineCloudDownload} from "react-icons/ai";
 import { useState } from "react"
+import { useEffect } from 'react';
+import { getSimpleFileById,deleteSignleFiles } from './services/axios';
+import Swal from 'sweetalert2';
 
-function SingleFilePreview({element ,handleShow,deletefiles}) {
+
+function SingleFilePreview ({id ,handleShow,isDeleted,setIsDeleted}) {
     const [showFile,setShowFile] = useState(false)
+    const [fileInfo , setFileInfo] = useState({})
+  
 
+    
+    useEffect(async()=>{
+      const response  = await getSimpleFileById(id)
+      if (response.success===true){
+        setFileInfo(response.data)
 
-    const button = showFile  ? <button className="eyebutton" onClick={()=> {setShowFile(false) ; handleShow("")}}><AiFillEyeInvisible/> </button>: < button className="eyebutton" onClick={()=>{handleShow(element.fileName); setShowFile(true)}}><AiFillEye/> </button>
+      }
+    }
 
-  return (
-    <div>
+,[])
+ 
+
+    
+   
+    const button =!(fileInfo
+    && Object.keys(fileInfo).length === 0
+    && Object.getPrototypeOf(fileInfo) === Object.prototype) && showFile  ? <button className='showeyes' onClick={()=> {setShowFile(false) ; handleShow("")}}><AiFillEyeInvisible/> </button>: < button className='showeyes' onClick={()=>{handleShow(fileInfo.filename); setShowFile(true)}}><AiFillEye/> </button>
+
+ 
+const handleDelete = ()=>{
+  Swal.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Delete it!'
+  }).then((result) => {
+    if (result.isConfirmed) {
+     deleteSignleFiles(id,"aaa").then((response) =>{
+       if(response.success===true) {
+        
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "bottom-left",
+          showConfirmButton: false,
+          timer: 1000,
+          timerProgressBar: true,
+        });
+
+        Toast.fire({
+          icon: "success",
+          title: response.data,
+        });
+        setIsDeleted(!isDeleted)
+       }else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+          
+        })
+       }
+     })
      
-     <tr>
-              <td>{element.originaleFileName}</td>
-              <td>{button}</td>
-            <td ><button  className="buttonpoubelle" onClick={()=>deletefiles(element.fileName)}><AiFillDelete/></button></td>  
-
-            </tr>
-           
 
 
-
-    </div>
-  )
+    }
+  })
 }
+  return (
+    <>
+   {!(fileInfo
+&& Object.keys(fileInfo).length === 0
+&& Object.getPrototypeOf(fileInfo) === Object.prototype) && (
+  <tr>
+  <td>
+   <label>{fileInfo.metadata.originalFileName}</label>
+  
+  
+  </td>  
+           {/* <td> <CheckBox ></CheckBox>{element.originaleFileName}</td> */}
+           <td >{button}</td>
+         <td ><button  className="buttonpoubelle" onClick={handleDelete}><AiFillCloseCircle/></button></td>  
+
+         </tr>
+        
+)}
+       
+           
+         
+        
+         
+            </>
+           
+  )
+ 
+  }
+
 
 export default SingleFilePreview
